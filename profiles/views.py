716d05434ec32e5fd4c4868aa556fb97813dc55f
michael-otieno <otieno.oduor.michael@gmail.com>
2024-07-31 23:36:55 +0300

    delete profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,7 +8,7 @@ from profiles.models import Profile
 from profiles.serializers import ProfileSerializer
 
 # Create your views here.
-class ProfileRetrieveView(RetrieveUpdateAPIView):
+class ProfileRetrieveView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ProfileSerializer
 
@@ -20,3 +20,15 @@ class ProfileRetrieveView(RetrieveUpdateAPIView):
         profile = self.get_object()
         serializer = self.serializer_class(profile)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user = profile.user
+        # Check and delete related objects if they exist
+        if profile.location:
+            profile.location.delete()
+        if profile.social_media:
+            profile.social_media.delete()
+        profile.delete()
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
