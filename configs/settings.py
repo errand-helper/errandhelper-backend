9f972c61cf6107791456de6f80af6b11e01e8f32
michael-otieno <otieno.oduor.michael@gmail.com>
2025-04-25 11:01:22 +0300

    myprojectuser

--- configs/settings.py
+++ configs/settings.py
@@ -106,9 +106,9 @@ DATABASES = {
          'ENGINE': 'django.db.backends.{}'.format(
              os.getenv('DATABASE_ENGINE', 'sqlite3')
          ),
-         'NAME': os.getenv('DATABASE_NAME', 'polls'),
-         'USER': os.getenv('DATABASE_USER', 'myprojectuser'),
-         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'NAME': os.getenv('DATABASE_NAME'),
+         'USER': os.getenv('DATABASE_USER'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
          'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
          'PORT': os.getenv('DATABASE_PORT', 5432),
      }
